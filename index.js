6f0644c6f96927cab4e0fc91e94e0751a806d00d
Diptabose <83805205+Diptabose@users.noreply.github.com>
2022-05-14 00:27:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path');
-
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(express.static(path.join(__dirname,'public')));
 
